89fa1b486823f097fe09c2a1c3a4d0380ba7c01b
toona <amanou.toona@gmail.com>
2020-08-01 20:31:43 +0900

    50_2

--- 50_2.py
+++ 50_2.py
@@ -13,67 +13,60 @@ for _ in range(M):
     d[t - 1][s - 1] = (_d, time)
 
 # dp 用意
-dp = [[None] * N for _ in range(1 << N)]
-counter = 0
+dp = [[[None, 0] for _ in range(N)] for _ in range(1 << N)]
 
 
 def res(s, v):
-    global counter
     # s: 訪問した店の集合, v: 今いる店, dp: 総移動距離 = 必要時間を記録する
     # 経路は封鎖前に渡り終えねばならない
-    # print(f'res called s:{format(s, "b")}, v:{v}')
 
-    if dp[s][v] is not None:
-        return dp[s][v]
+    if dp[s][v][0] is not None:
+        return
 
     if (s ^ (1 << v)) == 0:  # 終端の条件
         distance, time_limit = d[0][v]
         if distance is None:
-            dp[0][v] = float('inf')
-            return dp[0][v]
+            dp[s][v][0] = float('inf')
+            return
         if distance <= time_limit:
-            dp[0][v] = distance
-            return dp[0][v]
+            dp[s][v][0] = distance
+            dp[s][v][1] = 1
+            return
 
     ans = float('inf')
-    for u in range(N):  # u: 直前に店
+    for u in range(N):  # u: 直前の店
         if u == v:
             continue
 
         if (s >> u & 1) == 1:  # u に訪問済
-
-            # print(f"s: {format(s, 'b')}, u: {u}")
-
             distance, time_limit = d[u][v]
             if distance is None:
                 continue
 
-            if res(s ^ (1 << v), u) + distance > time_limit:  # 道が封鎖されている
+            res(s ^ (1 << v), u)
+
+            if dp[s ^ (1 << v)][u][0] + distance > time_limit:  # 道が封鎖されている
                 continue
 
-            if v == 0:
-                if res(s ^ (1 << v), u) + distance == ans:
-                    counter += 1
-                elif res(s ^ (1 << v), u) + distance < ans:
-                    counter = 1
+            if dp[s ^ (1 << v)][u][0] + distance == ans:
+                dp[s][v][1] += dp[s ^ (1 << v)][u][1]
+            elif dp[s ^ (1 << v)][u][0] + distance < ans:
+                dp[s][v][1] = dp[s ^ (1 << v)][u][1]
 
-            ans = min(ans, res(s ^ (1 << v), u) + distance)
+            ans = min(ans, dp[s ^ (1 << v)][u][0] + distance)
 
-        # if ans and v == 0:
-        #     counter += 1
+    dp[s][v][0] = ans
 
-    dp[s][v] = ans
-
-    return ans
+    return
 
 
-ans = res(2 ** N - 1, 0)
+res(2 ** N - 1, 0)
 
 # for i in range(len(dp)):
 #     print(dp[i])
 
 
-if ans == float('inf'):
+if dp[2 ** N - 1][0][0] == float('inf'):
     print('IMPOSSIBLE')
 else:
-    print(ans, counter)
+    print(dp[2 ** N - 1][0][0], dp[2 ** N - 1][0][1])
