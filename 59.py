6f6f7bb61815fb79820baea63b64e6f61a91fa25
toona notepc <amanou.toona@gmail.com>
2020-08-19 06:57:42 +0900

    2020/08/19

--- 59.py
+++ 59.py
@@ -2,5 +2,13 @@
 
 import sys
 
+N, K = map(int, sys.stdin.readline().strip().split())
+
+for n in range(N):
+    C, R = map(int, sys.stdin.readline().strip().split())
+    
+
+for k in range(K):
+    pass
 
 
